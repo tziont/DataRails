0ded95e5225575f7048bb5a274fdcdb38c006c51
tzion <tzion800@gmail.com>
2022-03-11 17:52:59 +0200

    completed the completed funk

--- HomeAssignment/src/app/components/wizard-modal/wizard-modal.component.ts
+++ HomeAssignment/src/app/components/wizard-modal/wizard-modal.component.ts
@@ -1,32 +1,51 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/Operators';
+import { Step } from 'src/app/typs/step';
 import { WizardConfigEndpointService } from 'src/app/services/wizard-config-endpoint.service';
-
-
 
 @Component({
   selector: 'app-wizard-modal',
   templateUrl: './wizard-modal.component.html',
-  styleUrls: ['./wizard-modal.component.scss']
+  styleUrls: ['./wizard-modal.component.scss'],
 })
 export class WizardModalComponent implements OnInit, OnDestroy {
-
-  private termination:Subject<null> = new Subject();
+  private termination: Subject<null> = new Subject();
   private terminate$: Observable<null> = this.termination.asObservable();
 
- res:any;
-  constructor(private wizardConfig : WizardConfigEndpointService) { }
+  wizard: any;
+  wizardSteps: Step[];
+  completedState: { id: number; isCompleted: boolean }[] = [];
+
+  constructor(private wizardConfig: WizardConfigEndpointService) {}
 
   ngOnInit(): void {
-  this.wizardConfig.getWizardConfig().pipe(takeUntil(this.terminate$)).subscribe(res => this.res = res)
+    this.wizardConfig
+      .getWizardConfig()
+      .pipe(takeUntil(this.terminate$))
+      .subscribe((res) => {
+        this.wizard = res;
+        this.wizardSteps = res.steps;
+        for (let i = 0; i < res.steps.length; i++) {
+          this.completedState.push({
+            id: i,
+            isCompleted: false,
+          });
+        }
+      });
   }
 
+  completed($event: any, i: number) {
+    console.log($event.target, i);
+    console.log($event.target);
+    if ($event.target.type === 'checkbox') {
+      this.completedState[i].isCompleted = !!$event.target.checked;
+    } else {
+      this.completedState[i].isCompleted = !!$event.target.value;
+    }
+  }
   ngOnDestroy(): void {
     this.termination.next();
     this.termination.complete();
   }
-
 }
-
-
